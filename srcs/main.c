f9b404c6e581b7615f87ac47d9774ccc4aec4afa
kram <markartyukh@gmail.com>
2021-01-27 09:25:08 +0300

    Added clion functionality

--- srcs/main.c
+++ srcs/main.c
@@ -5,7 +5,6 @@ void put_pixel(t_data *data, int x, int y, int color)
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel/ 8));
-
 	*(unsigned int *)dst = color;
 }
 
@@ -34,9 +33,7 @@ int create_trgb(int t, int r, int f, int b)
 	int res;
 
 	if (t < 0 || t > 255 || r < 0 || r > 255 || f < 0 || f > 255 || b < 0 || b > 255)
-	{
 		res = 0; // include ft_printf error message
-	}
 	else
 		res = (t << 8 | r << 8 | f << 8 | b);
 	return (res);
@@ -49,16 +46,16 @@ int	main(void)
 	t_data	img;
 
 	mlx = mlx_init();
-	mlx_window = mlx_new_window(mlx, 1920, 1080, "hello, world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	mlx_window = mlx_new_window(mlx, 1280, 720, "hello, world!");
+	img.img = mlx_new_image(mlx, 1280, 720);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	for (int y = 0; y < 1080; y++)
+	for (int y = 0; y < 720; y++)
 	{
-		for (int x = 0; x < 1980; x++)
+		for (int x = 0; x < 1280; x++)
 		{
-			if (pow((double)(x - 960), 2) + pow((double)(y - 540), 2) < pow(250, 2))
+			if (pow((double)(x - 640), 2) + pow((double)(y - 360), 2) < pow(250, 2))
 				put_pixel(&img, x, y, 0x00FF0000);
-			if (x > 900 && x < 1020 && y > 480 && y < 600)
+			if (x > 600 && x < 680 && y > 320 && y < 400)
 				put_pixel(&img, x, y, 0x000000FF);
 			// mlx_pixel_put(mlx, mlx_window, x, y, 0x00FF0000); apparently live render is slower than render in buffer
 		}
